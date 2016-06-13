abbe6a69a3c1e2a764c3138ddfd5d731b0bf267f
Vinicius Ribeiro <vinicius.ribeiro1@gmail.com>
2016-06-13 18:32:20 -0300

    Fixed spelling

--- non_linear_constraints.m
+++ non_linear_constraints.m
@@ -1,4 +1,4 @@
-function [c, ceq] = non_linear_contraints(y)
+function [c, ceq] = non_linear_constraints(y)
     c = (1/3) * exp(-2 * y(1) - 2 * y(2)) + ...
         (4/3) * exp(0.5 * y(2) - y(3)) - 1;
     ceq = [];
--- non_linear_constraints_log.m
+++ non_linear_constraints_log.m
@@ -1,4 +1,4 @@
-function [c, ceq] = non_linear_contraints_log(y)
+function [c, ceq] = non_linear_constraints_log(y)
     c = log((1/3) * exp(-2 * y(1) - 2 * y(2)) + ...
            (4/3) * exp(0.5 * y(2) - y(3)));
     ceq = [];
